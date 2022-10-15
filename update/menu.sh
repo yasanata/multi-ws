8512493da00014eac8b7f3ef9f97e8d510d4ee1f
YasaNata <yasanata@gmail.com>
2022-10-15 15:45:46 +0700

    Update menu.sh

--- update/menu.sh
+++ update/menu.sh
@@ -144,7 +144,7 @@ echo "$version_up" > /opt/.ver
 echo -e "$COLOR1│${NC}  $COLOR1[INFO]${NC} Successfully Up To Date!"
 echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}"
 echo -e "$COLOR1┌────────────────────── BY ───────────────────────┐${NC}"
-echo -e "$COLOR1│${NC}              • WWW.YASANATA.MY.ID •                $COLOR1│$NC"
+echo -e "$COLOR1│${NC}              • WWW.YASANATA.NINJA •                $COLOR1│$NC"
 echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}" 
 echo ""
 read -n 1 -s -r -p "  Press any key to back on menu"
@@ -227,7 +227,7 @@ else
 fi;
 echo -e "$COLOR1└─────────────────────────────────────────────────┘$NC"
 echo -e "$COLOR1┌────────────────────── BY ───────────────────────┐${NC}"
-echo -e "$COLOR1│${NC}              • WWW.YASANATA.MY.ID •            $COLOR1│$NC"
+echo -e "$COLOR1│${NC}              • WWW.YASANATA.NINJA •            $COLOR1│$NC"
 echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}" 
 echo -e ""
 echo -ne " Select menu : "; read opt
