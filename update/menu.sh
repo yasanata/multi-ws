7d6ad158321994649597c19f156140914ce63975
YasaNata <yasanata@gmail.com>
2023-07-30 21:21:51 +0700

    Update menu.sh

--- update/menu.sh
+++ update/menu.sh
@@ -216,7 +216,7 @@ datediff() {
 mai="datediff "$Exp" "$DATE""
 
 echo -e "$COLOR1┌─────────────────────────────────────────────────┐$NC"
-echo -e "$COLOR1│$NC Version     :${COLOR1} $(cat /opt/.ver) Latest Version${NC}"
+echo -e "$COLOR1│$NC Version     : Latest Version"
 echo -e "$COLOR1│$NC User Roles  : $uis"
 echo -e "$COLOR1│$NC Client Name : $Name"
 if [ $exp \> 1000 ];
